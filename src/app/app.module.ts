0c83727a74d2dfe380f936d82e9e77358b8c923c
guehochoi <ibaedoli@hotmail.com>
2019-04-18 22:35:09 -0700

    tutorials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -27,10 +28,13 @@ import { InMemoryDataService } from './in-memory-data.service';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+	// and returns simulated server responses.
+	// Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
     	InMemoryDataService, {dataEncapsulation: false})
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
